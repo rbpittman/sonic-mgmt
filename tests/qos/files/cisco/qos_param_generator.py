f228990231fb23971fa84ef071287d5f20610921
Randall Pittman <randallbpittman@gmail.com>
2023-03-29 14:17:33 +0000

    pre-commit formatting from master.

--- tests/qos/files/cisco/qos_param_generator.py
+++ tests/qos/files/cisco/qos_param_generator.py
@@ -1,5 +1,6 @@
 class QosParamCisco(object):
     SMALL_SMS_PLATFORMS = ["x86_64-8102_64h_o-r0"]
+
     def __init__(self, qos_params, duthost, bufferConfig):
         '''
         Initialize parameters all tests will use
@@ -15,7 +16,6 @@ class QosParamCisco(object):
         if "egress_lossy_pool" in self.bufferConfig["BUFFER_POOL"]:
             self.egress_pool_size = self.bufferConfig["BUFFER_POOL"]["egress_lossy_pool"]["size"]
         # Find SMS size
-        small_sms = 64 * (2 ** 20)
         self.is_large_sms = duthost.facts['platform'] not in self.SMALL_SMS_PLATFORMS
 
     def run(self):
