f7d76ea82d32035e02739004876e980bc5b81a09
Randall Pittman <randallbpittman@gmail.com>
2023-03-29 14:14:37 +0000

    Platform-based determination of sms size.

--- tests/qos/qos_sai_base.py
+++ tests/qos/qos_sai_base.py
@@ -1079,6 +1079,7 @@ class QosSaiBase(QosBase):
                 sys.path.append(sub_folder_dir)
             import qos_param_generator
             qpm = qos_param_generator.QosParamCisco(qosConfigs['qos_params'][dutAsic][dutTopo],
+                                                    duthost,
                                                     bufferConfig)
             qosParams = qpm.run()
         else:
